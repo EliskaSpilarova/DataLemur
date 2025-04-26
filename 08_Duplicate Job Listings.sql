5560be86034e7a0de8d789c58991faa91c55af78
Eliška Špilarová <164064165+EliskaSpilarova@users.noreply.github.com>
2025-04-26 19:28:11 +0200

    Create 08_Duplicate Job Listings.sql

---
+++ 08_Duplicate Job Listings.sql
@@ -0,0 +1,4 @@
+-- Assume you're given a table containing job postings from various companies on the LinkedIn platform. Write a query to retrieve the count of companies that have posted duplicate job listings.
+-- Definition:
+-- Duplicate job listings are defined as two job listings within the same company that share identical titles and descriptions.
+
